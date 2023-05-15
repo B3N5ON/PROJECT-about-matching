469923b96ae309ca196568471e7aa3919eb23a45
B3N5ON <128573027+B3N5ON@users.noreply.github.com>
2023-05-16 03:32:28 +0800

    Add files via upload

--- algorithm.h
+++ algorithm.h
@@ -8,100 +8,140 @@ unordered_map<wstring, wstring> studentMatching;
 
 unordered_map<wstring, wstring> schoolMatching;
 
-
-void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, unordered_map<wstring, wstring>& output)
+void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWSMAP& output)
 {
 	vector<wstring> freeStudent;
+
+	map<wstring, int> num;	//school's name, matching number
+
+	num[L"A"] = 2;
+	num[L"B"] = 3;
 
 	for (auto& it : schoolList)  //初始化
 	{
 		schoolMatching[it.first] = {};
 	}
-	for (auto& it : studentList)
-	{
-		studentMatching[it.first] = {};
 
-		freeStudent.push_back(it.first);
-	}
+	bool noFreeStudent = false, noOneWantHim = false, noSize = false;
 
-	bool noFreeStudent = false, noOneWantHim = false;
-
-	map<wstring, wstring > current;
-
-	while (!freeStudent.empty())		//每個申請者都在名單上 or 他申請的學校皆拒絕他
+	while (!noSize)
 	{
 		for (auto& it : studentList)
 		{
-			wstring student = it.first;
+			studentMatching[it.first] = {};
 
-			int favorite = 0, rejected = 0;
+			freeStudent.push_back(it.first);
+		}
 
-			if (studentMatching[student].empty())
+		map<wstring, wstring > current;
+
+		while (!freeStudent.empty())		//每個申請者都在名單上 or 他申請的學校皆拒絕他
+		{
+			for (auto& it : studentList)
 			{
-				while (it.second.size())		//學生志願序中的學校
+				if (freeStudent.empty())
+					break;
+
+				wstring student = it.first;
+
+				int favorite = 0, rejected = 0;
+
+				if (studentMatching[student].empty())
 				{
-					wstring school = it.second[favorite];
-
-					if (find(schoolList[school].begin(), schoolList[school].end(), student) == schoolList[school].end()) //如果學校清單沒有這位學生
+					while (it.second.size())		//學生志願序中的學校
 					{
-						break;
-					}
+						wstring school = it.second[favorite];
 
-					if (schoolMatching[school].empty())
-					{
-						schoolMatching[school] = student;
-
-						studentMatching[student] = school;
-
-						current[school] = student;
-
-						favorite = 0;
-
-						freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
-
-						break;
-					}
-					else
-					{
-						auto currentIndex = current.find(school);
-
-						wstring currentStudent = currentIndex->second;
-
-						int currentStundetIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), currentStudent));
-
-						int newStudentIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), student));
-
-						if (currentStundetIndex > newStudentIndex)
+						if (num[school] != 0)		//如果學校人數還沒招完
 						{
-							schoolMatching[school] = student;
+							if (find(schoolList[school].begin(), schoolList[school].end(), student) != schoolList[school].end())  ///學校清單找的到學生
+							{
+								if (schoolMatching[school].empty())
+								{
+									schoolMatching[school] = student;
 
-							studentMatching[student] = school;
+									studentMatching[student] = school;
 
-							studentMatching[currentStudent] = L"";
+									current[school] = student;
 
-							freeStudent.push_back(currentStudent);
+									favorite = 0;
 
-							favorite = 0;
+									freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
-							current[school] = student;
+									break;
+								}
+								else
+								{
+									auto currentIndex = current.find(school);
 
-							freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
+									wstring currentStudent = currentIndex->second;
 
-							break;
+									int currentStundetIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), currentStudent));
+
+									int newStudentIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), student));
+
+									if (currentStundetIndex > newStudentIndex)
+									{
+										schoolMatching[school] = student;
+
+										studentMatching[student] = school;
+
+										studentMatching[currentStudent] = L"";
+
+										freeStudent.push_back(currentStudent);
+
+										favorite = 0;
+
+										current[school] = student;
+
+										freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
+
+										break;
+									}
+
+									else
+									{
+										favorite++;			//find next school
+
+										rejected++;
+
+										if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
+										{
+											noOneWantHim = true; /**/
+
+											freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
+
+											break;
+										}
+									}
+								}
+							}
+							else
+							{	///學校清單找不到學生(成績不夠格)
+								favorite++;
+
+								rejected++;
+
+								if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
+								{
+									noOneWantHim = true; /**/
+
+									freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
+
+									break;
+								}
+							}
 						}
 						else
-						{
-							favorite++;			//find next school
+						{	//學校已招滿
+							favorite++;
 
 							rejected++;
 
 							if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
 							{
-								noOneWantHim = true;
-							}
+								noOneWantHim = true; /**/
 
-							if (noOneWantHim)
-							{
 								freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
 								break;
@@ -111,9 +151,36 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, unordered
 				}
 			}
 		}
-	}
-	for (auto& entry : studentMatching)
-	{
-		output[entry.first] = entry.second;
+
+		for (auto& entry : studentMatching)
+		{
+			output[entry.first].push_back(entry.second);
+
+			schoolMatching[entry.second] = {};
+
+			if (!entry.second.empty())
+			{
+				schoolList[entry.second].erase(find(schoolList[entry.second].begin(), schoolList[entry.second].end(), entry.first));	///把已經配對到的學生從學校列表中刪除
+
+				if(num[entry.second] != 0)
+					num[entry.second]--;
+
+				studentMatching[entry.first] = {}; //初始化
+
+				int text = 0;
+			}
+		}
+
+		int count = 0;
+
+		for (auto& entry : num)
+		{
+			if (entry.second == 0)
+			{
+				count++;
+			}
+			if (count == num.size())
+				noSize = true;
+		}
 	}
 }
