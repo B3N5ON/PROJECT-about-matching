c3811b26e2868abd2bc73ef23a16429d220fc855
B3N5ON <128573027+B3N5ON@users.noreply.github.com>
2023-05-17 15:24:45 +0800

    Add files via upload

--- algorithm.h
+++ algorithm.h
@@ -8,21 +8,27 @@ unordered_map<wstring, wstring> studentMatching;
 
 unordered_map<wstring, wstring> schoolMatching;
 
-void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWSMAP& output)
+void stableMatching(map<wstring, student> studentList, map<wstring, school> schoolList, UNORDERWSMAP& output)
 {
 	vector<wstring> freeStudent;
 
 	map<wstring, int> num;	//school's name, matching number
 
-	num[L"A"] = 2;
-	num[L"B"] = 3;
-
 	for (auto& it : schoolList)  //初始化
 	{
 		schoolMatching[it.first] = {};
+
+		num[it.first] = it.second.Number;
+
+		if (num[it.first] > it.second.stdList.size())		//如果招收人數小於可錄取人數
+		{
+			num[it.first] = it.second.stdList.size();
+		}
 	}
 
-	bool noFreeStudent = false, noOneWantHim = false, noSize = false;
+	bool noFreeStudent = false, noSize = false;
+
+	map<wstring, bool> noOneWantHim;
 
 	while (!noSize)
 	{
@@ -31,6 +37,8 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 			studentMatching[it.first] = {};
 
 			freeStudent.push_back(it.first);
+
+			noOneWantHim[it.first] = false;
 		}
 
 		map<wstring, wstring > current;
@@ -48,13 +56,13 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 				if (studentMatching[student].empty())
 				{
-					while (it.second.size())		//學生志願序中的學校
+					while (it.second.Rank.size())		//學生志願序中的學校
 					{
-						wstring school = it.second[favorite];
+						wstring school = it.second.Rank[favorite];
 
 						if (num[school] != 0)		//如果學校人數還沒招完
 						{
-							if (find(schoolList[school].begin(), schoolList[school].end(), student) != schoolList[school].end())  ///學校清單找的到學生
+							if (find(schoolList[school].stdList.begin(), schoolList[school].stdList.end(), student) != schoolList[school].stdList.end())  ///學校清單找的到學生
 							{
 								if (schoolMatching[school].empty())
 								{
@@ -68,6 +76,8 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 									freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
+									noOneWantHim[student] = false;
+
 									break;
 								}
 								else
@@ -76,9 +86,9 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 									wstring currentStudent = currentIndex->second;
 
-									int currentStundetIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), currentStudent));
+									int currentStundetIndex = distance(schoolList[school].stdList.begin(), find(schoolList[school].stdList.begin(), schoolList[school].stdList.end(), currentStudent));
 
-									int newStudentIndex = distance(schoolList[school].begin(), find(schoolList[school].begin(), schoolList[school].end(), student));
+									int newStudentIndex = distance(schoolList[school].stdList.begin(), find(schoolList[school].stdList.begin(), schoolList[school].stdList.end(), student));
 
 									if (currentStundetIndex > newStudentIndex)
 									{
@@ -96,6 +106,8 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 										freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
+										noOneWantHim[student] = false;
+
 										break;
 									}
 
@@ -105,9 +117,13 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 										rejected++;
 
-										if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
+										if (rejected == studentList[student].Rank.size())		//被拒絕數等於他所申請的數量
 										{
-											noOneWantHim = true; /**/
+											if (noOneWantHim[student])		//之前就有人不要了
+											{
+												break;
+											}
+											noOneWantHim[student] = true; /**/
 
 											freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
@@ -122,9 +138,14 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 								rejected++;
 
-								if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
+								if (rejected == studentList[student].Rank.size())		//被拒絕數等於他所申請的數量
 								{
-									noOneWantHim = true; /**/
+									if (noOneWantHim[student])		//之前就有人不要了
+									{
+										break;
+									}
+
+									noOneWantHim[student] = true; /**/
 
 									freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
@@ -138,9 +159,14 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 							rejected++;
 
-							if (rejected == studentList[student].size())		//被拒絕數等於他所申請的數量
+							if (noOneWantHim[student])		//之前就有人不要了
 							{
-								noOneWantHim = true; /**/
+								break;
+							}
+
+							if (rejected == studentList[student].Rank.size())		//被拒絕數等於他所申請的數量
+							{
+								noOneWantHim[student] = true; /**/
 
 								freeStudent.erase(find(freeStudent.begin(), freeStudent.end(), student));
 
@@ -154,13 +180,15 @@ void stableMatching(UNORDERWSMAP studentList, UNORDERWSMAP schoolList, UNORDERWS
 
 		for (auto& entry : studentMatching)
 		{
-			output[entry.first].push_back(entry.second);
+			output[entry.first].push_back(entry.second);   //輸出
 
 			schoolMatching[entry.second] = {};
 
 			if (!entry.second.empty())
 			{
-				schoolList[entry.second].erase(find(schoolList[entry.second].begin(), schoolList[entry.second].end(), entry.first));	///把已經配對到的學生從學校列表中刪除
+				schoolList[entry.second].stdList.erase(find(schoolList[entry.second].stdList.begin(), 
+
+					schoolList[entry.second].stdList.end(), entry.first));	///把已經配對到的學生從學校列表中刪除
 
 				if(num[entry.second] != 0)
 					num[entry.second]--;
---
+++ project.cpp
@@ -0,0 +1,330 @@
+#include "Headerfun.h"
+#include "algorithm.h"
+#include <locale>	
+#include <codecvt>
+
+// 爾後還要建立學校代碼對上該校名稱、科系的檔案
+vector<WS> rankList;			///志願序排列
+
+map<wstring, student> StudentMap;			//Map<考生代碼，五科成績資料>
+
+map<wstring, vector<int>> FiveStandards;		//map<科目名稱，五標>
+
+map<wstring, school> SchoolDetail;			//map<學校ID, 校細分則詳細資料>
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+
+	locale loc(locale(), new codecvt_utf8<wchar_t>);
+
+	wcout.imbue(loc);
+
+	SetConsoleCP(65001);
+
+	SetConsoleOutputCP(65001);
+
+	buiild_student_rank(rankList);		//Get the student's rank type is vector<vector<string>>
+
+	build_student_list(StudentMap);			//Get the student's list type is map<wstring,  class student>
+
+	FiveStanderMap(FiveStandards);				//Get the Stander of grade and convert it to the integer
+
+	build_school_rank(SchoolDetail,FiveStandards);			//Put in the map<wstring, school> if the 5 stander convert chinese to the interger 
+
+	//build school's preferance of the students
+	for (int i = 0; i < rankList.size(); i++)
+	{
+		auto st = StudentMap.find(rankList[i][0]);		
+		//get student detail
+		for (int j = 1; j < rankList[i].size(); j++)  //first is ID, so loop start on the 1
+		{
+			auto sch = SchoolDetail.find(rankList[i][j]);		//get school detail
+			//Determine whether student's grade are confrom with school's grade
+			//五標篩選
+			//如果國文成績大於給的國文標準或者學校不取國文(國文的倍率為0)
+			if (st->second.Chinese >= sch->second.ChineseG || sch->second.ChineseG == 0)
+			{
+				if (st->second.English >= sch->second.EnglishG || sch->second.EnglishG == 0) 
+				{
+					if (st->second.MathA >= sch->second.MathAG || sch->second.MathAG == 0) 
+					{
+						if (st->second.MathB >= sch->second.MathBG || sch->second.MathBG == 0)
+						{
+							if (st->second.Social >= sch->second.SocialG || sch->second.SocialG == 0)
+							{
+								if (st->second.Nature >= sch->second.NatureG || sch->second.NatureG == 0)
+								{
+									sch->second.stdList.push_back(st->first);			//the sort at here haven't sortted regularly
+								}
+							}
+						}
+					}
+				}								
+			}									
+		}
+	}
+	//sorting stdList let the sort inside is descending(high -> low score) to complete school's preferance
+	for (auto& t : SchoolDetail) {
+		int wantNum = static_cast<int>(t.second.stdList.size());		///得到過分數標準的人數
+
+		int auditionNum = 0;
+
+		vector<double> ratioArray = { t.second.chRatio, t.second.EnRatio, t.second.MARatio, t.second.MBRatio, t.second.SoRatio, t.second.NaRatio };//might add more
+
+		vector<double> ratio5Array = { t.second.chRatio, t.second.EnRatio, t.second.MARatio, t.second.MBRatio, t.second.SoRatio, t.second.NaRatio };
+		
+		multimap<double, size_t> m;		//得到五科相同比率的位置，若沒有相同size為1
+
+		map<double, vector<int>> sameRat;
+
+		for (size_t i = 0; i < ratio5Array.size(); i++) {
+			if(ratio5Array[i] != 0)
+				m.insert({ ratio5Array[i], i });
+		}
+		for (auto it = m.begin(); it != m.end();)
+		{
+			auto p = m.equal_range(it->first);
+
+			vector<int> temp;
+
+			while (p.first != p.second)
+			{
+				temp.push_back(static_cast<int>(p.first++->second));
+			}
+			sameRat.insert(pair<double, vector<int>>(it->first, temp));//(比率，相同比率之位置)
+
+			it = p.second;
+		}
+		//知道有幾種不同的考科，比率相同算在一起
+		for (auto it = sameRat.rbegin(); it != sameRat.rend(); ++it)			//因為map已經排列成key由小到大，故用rbegin從尾巴(最大數)開始看
+		{		
+			auditionNum = int(ceil(double(t.second.Number) * it->first)); //若比率有小數點，則乘完後無條件進位
+
+			vector<double> SubjectGrade;			
+
+			if (it->second.size() == 1) //若沒有相同的倍率
+			{
+				int tempG = it->second[0];
+
+				for (int i = 0; i < t.second.stdList.size(); i++)
+				{
+					double temp;
+
+					auto k = StudentMap.find(t.second.stdList[i]);
+
+					double s_sub = it->first;		//拿到該科的倍率
+
+					switch (tempG)
+					{
+					case 0:
+						temp = k->second.Chinese * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					case 1:
+						temp = k->second.English * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					case 2:
+						temp = k->second.MathA * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					case 3:
+						temp = k->second.MathB * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					case 4:
+						temp = k->second.Social * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					case 5:
+						temp = k->second.Nature * s_sub;
+
+						SubjectGrade.push_back(temp);
+
+						continue;
+					}
+				}
+			}
+			//如果有相同比率，則相同比率之乘積相加
+			else if(it->second.size() > 1)
+			{
+				vector<int> mutiSub = it->second;
+
+				for (int i = 0; i < t.second.stdList.size(); i++)
+				{
+					auto k = StudentMap.find(t.second.stdList[i]);		
+
+					double SumSubs = 0;
+
+					for (int p : mutiSub)  //get which subs
+					{
+						if (p == 0) 
+						{
+							SumSubs += k->second.Chinese;
+						}
+						else if (p == 1) 
+						{
+							SumSubs += k->second.English;
+						}						
+						else if (p == 2) 
+						{
+							SumSubs += k->second.MathA;
+						}							
+						else if (p == 3)
+						{
+							SumSubs += k->second.MathB;
+						}							
+						else if (p == 4)
+						{
+							SumSubs += k->second.Social;
+						}							
+						else if (p == 5)
+						{
+							SumSubs += k->second.Nature;
+						}
+					}
+					double ratio = it->first;
+
+					SumSubs *= ratio;	// ratio * ( same ratio's subjects sum)
+
+					SubjectGrade.push_back(SumSubs);
+				}
+			}
+			///Use Bubble Sort to sort student's ID, sum of score.
+			for (int j = SubjectGrade.size()-1; j>0; j--)
+			{
+				for (int k = 0; k <= j - 1; k++)
+				{
+					if (SubjectGrade[k] < SubjectGrade[k + 1])
+					{
+						swap(SubjectGrade[k], SubjectGrade[k + 1]);
+
+						swap(t.second.stdList[k], t.second.stdList[k + 1]);
+					}
+				}
+			}
+
+			if (t.second.stdList.size() > auditionNum)
+			{
+				int LastStudentGrade = SubjectGrade[auditionNum - 1];
+
+				vector<int> AddresReg;		//紀錄相同分數的位置
+
+				for (int k = 0; k < SubjectGrade.size(); k++)
+				{
+					if (SubjectGrade[k] == LastStudentGrade)
+					{
+						AddresReg.push_back(k);		//if same grade then put the address into vector
+					}
+				}
+				int a = AddresReg.back();			//get the last element's value, to know how many people have same grade
+
+				t.second.stdList.erase(t.second.stdList.begin() + a, t.second.stdList.end());		//then erase the people whose score is less than the last person's score
+			}
+		}
+	}
+
+//Do algorithm
+//已經拿到school對學生的preference和原先學生對school的preference,現在開始做演算法的排序
+//school's preference has completed its priority of student, and those student has admissed  minimum requirement
+
+	unordered_map<wstring, vector<wstring>> s_rank;
+
+	for (int i = 0; i < rankList.size(); i++)
+	{
+		vector<wstring> temp_rank = rankList[i];
+
+		temp_rank.erase(temp_rank.begin());
+
+		wstring sID = rankList[i][0];
+
+		auto it = StudentMap.find(sID);
+
+		it->second.Rank = temp_rank;
+	}
+
+	UNORDERWSMAP match;
+
+	///*This is testing data */
+	//unordered_map<wstring, vector<wstring>> Test_data_sc = {
+	//{L"A", {L"a", L"b", L"c", L"d",L"e",L"f"}},
+	//{L"B", {L"f", L"e", L"d", L"c",L"b",L"a"}}
+	//};
+	//unordered_map<wstring, vector<wstring>> Test_data_sd = {
+	//{L"a", {L"A", L"B"}},
+	//{L"b", {L"A", L"B"}},
+	//{L"c", {L"B", L"A"}},
+	//{L"d", {L"B", L"A"}},
+	//	{L"e",{L"A",L"B"}},
+	//	{L"f", {L"B", L"A"}},
+	//	};
+
+	stableMatching(StudentMap, SchoolDetail, match);
+
+for (auto& it2 : match)
+{
+	auto sTN = StudentMap.find(it2.first);
+
+	wstring studentname = sTN->second.name;
+
+	wstring schoolName = L"NUN";
+
+	wcout << studentname << L"- - - -";
+
+	for (auto& it3 : it2.second)
+	{
+		if (it3 != L"")
+		{
+			auto sCN = SchoolDetail.find(it3);
+
+			schoolName = sCN->second.SchoolName;
+
+			wcout << schoolName << L"	";
+		}
+
+	}
+	wcout << endl;
+}
+
+//for (auto& it : match)
+//{
+//	auto stN = StudentMap.find(it.first);
+//
+//	wstring studentName = stN->second.name;
+//
+//	wstring schoolName = L"NUN";
+//
+//	if (it.second != L"") {
+//		auto scN = SchoolDetail.find(it.second);
+//
+//		schoolName = scN->second.SchoolName;
+//	}
+//	wcout << studentName << L" -- " << schoolName << endl;
+//}
+
+wcout << L"\n" << L"學校對學生的順位" << endl;
+	for (auto& it : SchoolDetail) {
+		wcout << it.second.SchoolName << L":";
+		for (int i = 0; i < it.second.stdList.size(); i++) {
+			auto it2 = StudentMap.find(it.second.stdList[i]);
+			wcout << it2->second.name << L" ";
+		}
+		wcout << L"\n";
+	}
+
+	SetConsoleCP(950);
+
+	SetConsoleOutputCP(950);
+}
+
